f2a1eb8ab8b87a6bde1aacc0ad4f730bbd8ae118
Lumio25 <paulamartaa17@gmail.com>
2025-05-29 12:26:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ const proyectosData = {
     "3": {
         titulo: "Hotel Intro Barranco",
         estado: "Entregado",
-        descripcion: "Rehabilitación completa de las instalaciones eléctricas y sanitarias para el Hotel Intro en Barranco, Lima. El proyecto incluyó la renovación del sistema eléctrico, instalación de tablero eléctrico, sistemas de iluminación y redes de datos; así como la implementación de nuevas griferías, baños, duchas y sistema de agua caliente en habitaciones y áreas comunes,junto con trabajos de reparación y mejora en la casona.", 
+        descripcion: "Rehabilitación completa de las instalaciones eléctricas y sanitarias para el Hotel Intro en Barranco, Lima. El proyecto incluyó la renovación del sistema eléctrico, instalación de tablero eléctrico, sistemas de iluminación y redes de datos; así como la implementación de nuevas griferías, baños, duchas y sistema de agua caliente en habitaciones y áreas comunes, junto con trabajos de reparación y mejora en la casona.", 
         ubicacion: "Barranco, Lima",
         año: "2025",
         servicios: [
